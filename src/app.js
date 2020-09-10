5f6259d0d69ea2278dd6b18479842a508fb975e1
Bez <bezlash@gmail.com>
2020-09-10 11:39:27 +0700

    created priovate routes

--- src/app.js
+++ src/app.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import AppRouter from './routers/AppRouter';
+import AppRouter, { history } from './routers/AppRouter';
 import configureStore from './store/consfigureStore'
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import { startSetExpenses } from './actions/expenses';
-import { setTextFilter } from './actions/filters';
+import { login, logout } from './actions/auth';
 import getVisibleExpenses from './selectors/expenses';
-import './firebase/firebase';
+import { firebase } from './firebase/firebase';
 
 const store = configureStore();
 store.subscribe(() => {
@@ -22,15 +22,36 @@ const jsx = (
   </Provider>
 )
 
+let hasRendered = false;
+const renderApp = () => {
+  if(!hasRendered) {
+    ReactDOM.render(jsx, document.getElementById('app'));
+    hasRendered = true;
+  }
+}
+
 // store.dispatch(addExpense({description: 'Water bill', amount: 4500}));
 // store.dispatch(addExpense({description: 'Gas bill', createdAt:1596240000}));
 // store.dispatch(addExpense({description: 'Rent', amount: 109500}));
 
 ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
 
-store.dispatch(startSetExpenses()).then(() => {
-  ReactDOM.render(jsx, document.getElementById('app'));
-});
 
+
+firebase.auth().onAuthStateChanged((user) => {
+  if(user) {
+    store.dispatch(login(user.uid));
+    store.dispatch(startSetExpenses()).then(() => {
+      renderApp();
+      if(history.location.pathname === '/') {
+        history.push('/dashboard');
+      }
+    });
+  } else {
+    store.dispatch(logout());
+    renderApp();
+    history.push('/');
+  }
+});
 
 
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,25 +1,29 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch, Link, NavLink, withRouter } from 'react-router-dom';
+import { Router, Route, Switch, Link, NavLink, withRouter } from 'react-router-dom';
 import NotFoundPage from '../components/NotFoundPage';
 import HelpPage from '../components/HelpPage';
 import AddExpensePage from '../components/AddExpensePage';
 import EditExpensePage from '../components/EditExpensePage';
 import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
-import Header from '../components/Header';
+import LoginPage from '../components/LoginPage';
+import { createBrowserHistory } from 'history';
+import PrivateRoute from './PrivateRoute';
+
+export const history = createBrowserHistory();
 
 const AppRouter = () => (
-  <BrowserRouter>
+  <Router history={history}>
     <div>
-      <Header />
       <Switch>
-        <Route path="/" component={ExpenseDashboardPage} exact={true} />
-        <Route path="/create" component={withRouter(AddExpensePage)} />
-        <Route path="/edit/:id" component={withRouter(EditExpensePage)} />
+        <Route path="/" component={LoginPage} exact={true} />
+        <PrivateRoute path="/dashboard" component={ExpenseDashboardPage} exact={true} />
+        <PrivateRoute path="/create" component={withRouter(AddExpensePage)} />
+        <PrivateRoute path="/edit/:id" component={withRouter(EditExpensePage)} />
         <Route path="/help" component={HelpPage} />
         <Route component={NotFoundPage} />
       </Switch>
     </div>
-  </BrowserRouter>
+  </Router>
 );
 
 export default AppRouter;
