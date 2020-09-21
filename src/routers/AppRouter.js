eb232dc9f5ac3aca86592c8cd9a2e6a2c1766891
Bez <bezlash@gmail.com>
2020-09-21 14:38:02 +0700

    authentication

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Router, Route, Switch, Link, NavLink, withRouter } from 'react-router-dom';
 import NotFoundPage from '../components/NotFoundPage';
-import HelpPage from '../components/HelpPage';
 import AddExpensePage from '../components/AddExpensePage';
 import EditExpensePage from '../components/EditExpensePage';
 import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
 import LoginPage from '../components/LoginPage';
 import { createBrowserHistory } from 'history';
 import PrivateRoute from './PrivateRoute';
+import PublicRoute from './PublicRoute';
 
 export const history = createBrowserHistory();
 
@@ -15,11 +15,10 @@ const AppRouter = () => (
   <Router history={history}>
     <div>
       <Switch>
-        <Route path="/" component={LoginPage} exact={true} />
+        <PublicRoute path="/" component={LoginPage} exact={true} />
         <PrivateRoute path="/dashboard" component={ExpenseDashboardPage} exact={true} />
         <PrivateRoute path="/create" component={withRouter(AddExpensePage)} />
         <PrivateRoute path="/edit/:id" component={withRouter(EditExpensePage)} />
-        <Route path="/help" component={HelpPage} />
         <Route component={NotFoundPage} />
       </Switch>
     </div>
